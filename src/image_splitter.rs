e74131253d173be93af50d2ceb1d2c4a51a15a00
Andy Bruce <andycbruce@gmail.com>
2023-03-19 20:42:16 -0700

    90% functional

--- src/image_splitter.rs
+++ src/image_splitter.rs
@@ -1,17 +1,32 @@
 use crate::andy_vectors::Vec2D;
+use std::cmp;
 use std::collections::VecDeque;
 
-//BFS is better than DFS cause doing it recursive causes a stackoverflow for large chunks
+pub struct FloodedChunkInfo {
+    pub val: u32,
+    pub num_pixels: u64,
+    pub top_left: (usize, usize),
+    pub bot_right: (usize, usize),
+}
+
+//BFS is better than DFS cause doing it DFS with recursive causes a stackoverflow for large chunks, using a
+//queue and a loop avoids too many stack frames
 fn flood_from_pixel_bfs(
     mask: &Vec2D<bool>,
     start_pos: (usize, usize),
     value: u32,
     output: &mut Vec2D<Option<u32>>,
-) {
+) -> FloodedChunkInfo {
     assert!(mask.w == output.w && mask.h == output.h);
 
     let mut to_search: VecDeque<(usize, usize)> = VecDeque::new();
     to_search.push_back(start_pos);
+
+    let mut top_left_x: usize = start_pos.0;
+    let mut top_left_y: usize = start_pos.1;
+    let mut bot_right_x: usize = start_pos.0;
+    let mut bot_right_y: usize = start_pos.1;
+    let mut num_pixels: u64 = 0;
 
     while !to_search.is_empty() {
         let pos: (usize, usize) = to_search.pop_front().unwrap();
@@ -23,9 +38,15 @@ fn flood_from_pixel_bfs(
                 continue;
             }
             output[pos] = Some(value);
+            num_pixels += 1;
 
             let x: usize = pos.0;
             let y: usize = pos.1;
+
+            top_left_x = cmp::min(top_left_x, x);
+            top_left_y = cmp::min(top_left_y, y);
+            bot_right_x = cmp::max(bot_right_x, x);
+            bot_right_y = cmp::max(bot_right_y, y);
 
             for dy in -1i64..=1 {
                 for dx in -1i64..=1 {
@@ -46,9 +67,16 @@ fn flood_from_pixel_bfs(
             assert!(output[pos].is_none());
         }
     }
+
+    FloodedChunkInfo {
+        val: value,
+        num_pixels,
+        top_left: (top_left_x, top_left_y),
+        bot_right: (bot_right_x, bot_right_y),
+    }
 }
 
-pub fn flood_mask(mask: Vec2D<bool>) -> Vec2D<Option<u32>> {
+pub fn flood_mask(mask: Vec2D<bool>) -> (Vec<FloodedChunkInfo>, Vec2D<Option<u32>>) {
     let mut output: Vec2D<Option<u32>> = Vec2D {
         data: vec![None; mask.w * mask.h],
         w: mask.w,
@@ -57,14 +85,17 @@ pub fn flood_mask(mask: Vec2D<bool>) -> Vec2D<Option<u32>> {
 
     let mut curr_val = 0;
 
+    let mut chunks: Vec<FloodedChunkInfo> = vec![];
+
     for y in 0..mask.h {
         for x in 0..mask.w {
             let pos = (x, y);
             if mask[pos] && output[pos].is_none() {
-                flood_from_pixel_bfs(&mask, (x, y), curr_val, &mut output);
+                let chunk = flood_from_pixel_bfs(&mask, (x, y), curr_val, &mut output);
+                chunks.push(chunk);
                 curr_val += 1;
             }
         }
     }
-    output
+    (chunks, output)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,22 @@ mod image_reader;
 mod image_splitter;
 use crate::andy_vectors::Vec2D;
 use std::iter::zip;
+
+fn draw_square(
+    img: &mut Vec2D<(u8, u8, u8, u8)>,
+    top_left: (usize, usize),
+    bot_right: (usize, usize),
+) {
+    for x in top_left.0..bot_right.0 {
+        img[(x, top_left.1)] = (255, 0, 0, 255);
+        img[(x, bot_right.1)] = (255, 0, 0, 255);
+    }
+    for y in top_left.1..bot_right.1 {
+        img[(top_left.0, y)] = (255, 0, 0, 255);
+        img[(bot_right.0, y)] = (255, 0, 0, 255);
+    }
+}
+
 fn main() {
     let pixels: Vec2D<(u8, u8, u8, u8)> = image_reader::read_image_into_vec("plant4.jpg");
 
@@ -19,18 +35,15 @@ fn main() {
         h: pixels.h,
     };
 
-    let masked_image: Vec2D<(u8, u8, u8, u8)> = Vec2D {
+    let mut masked_image: Vec2D<(u8, u8, u8, u8)> = Vec2D {
         data: zip(pixels.data.iter(), image_mask.data.iter())
-            .map(|(&pixel, &is_masked)| if is_masked { pixel } else { (255, 0, 255, 255) })
+            .map(|(&pixel, &is_masked)| if is_masked { pixel } else { (0, 0, 0, 255) })
             .collect(),
         w: pixels.w,
         h: pixels.h,
     };
 
-    let masked_image_output = image_reader::make_image_from_vec(masked_image);
-    masked_image_output.save("out.png").unwrap();
-
-    let flooded_mask = image_splitter::flood_mask(image_mask);
+    let (chunks, flooded_mask) = image_splitter::flood_mask(image_mask);
 
     let colors: [(u8, u8, u8, u8); 6] = [
         (255, 0, 0, 255),
@@ -46,7 +59,7 @@ fn main() {
             .data
             .iter()
             .map(|&flood_val| match flood_val {
-                None => (0, 0, 0, 0),
+                None => (0, 0, 0, 255),
                 Some(val) => colors[(val % 6) as usize],
             })
             .collect(),
@@ -54,6 +67,12 @@ fn main() {
         h: pixels.h,
     };
 
+    for chunk in chunks {
+        draw_square(&mut masked_image, chunk.top_left, chunk.bot_right);
+    }
+
     let chunk_colors_image_output = image_reader::make_image_from_vec(chunk_colors);
     chunk_colors_image_output.save("chunks.png").unwrap();
+    let masked_image_output = image_reader::make_image_from_vec(masked_image);
+    masked_image_output.save("out.png").unwrap();
 }
